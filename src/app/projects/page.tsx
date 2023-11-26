5af458fb457b547622a5c18fc86430b3e4e26e7a
hamidreza <hamidrezaramzani80@gmail.com>
2023-11-26 16:45:58 +0330

    add basic workflows

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,20 @@
+import MainLayout from "@/components/MainLayout/main-layout.component";
+import { wordBook } from "@/constants/wordBook";
+import { Metadata } from "next";
+
+export default function ProjectsPage() {
+  return (
+    <MainLayout title={wordBook.view.projects.title.en}>
+      <div className="w-1/3">
+        <img src="/" alt="This is test" />
+        <h3>Project Title</h3>
+        <p>This is project description</p>
+      </div>
+    </MainLayout>
+  );
+}
+
+export const metadata: Metadata = {
+  title: wordBook.view.projects.title.en,
+  description: "These are my projects that are made...",
+};
