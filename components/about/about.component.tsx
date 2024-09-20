2e3af0cfd5a577911218eb4c988e52a9397439de
Hamidreza <hamidrezaramzani80@gmail.com>
2024-09-21 02:10:29 +0330

    feat: change about section style

--- components/about/about.component.tsx
+++ components/about/about.component.tsx
@@ -8,17 +8,15 @@ export const About = () => {
     <div className="section w-full flex justify-center align-middle h-screen">
       <div className="flex justify-center w-full gap-5">
         <div className="flex flex-col w-1/2 gap-5 align-middle">
-          <div className="flex align-middle justify-center gap-6">
-            <Image src="/alien.png" width={400} alt="Alien" height={400} />
-            <div className="flex flex-col justify-center">
+          <div className="flex flex-col justify-center items-center">
+            <Image src="/alien.png" width={250} alt="Alien" height={250} />
+            <div className="flex flex-col text-center">
               <TypeAnimation
-                className="raleway-bold text-5xl mb-5"
+                className="raleway-bold text-xl mt-3 mb-1"
                 sequence={[wordbook.about.fullName]}
                 repeat={3}
               />
-              <p className="raleway-regular mt-1">
-                {wordbook.about.description}
-              </p>
+              <p className="raleway-regular">{wordbook.about.description}</p>
             </div>
           </div>
         </div>
--- utils/wordbook.ts
+++ utils/wordbook.ts
@@ -5,14 +5,6 @@ export const wordbook: {
 } = {
   about: {
     fullName: "Hamidreza Ramzani",
-    description: `I'm Hamidreza, a 22-year-old web developer with a strong passion
-              for technology and coding. I specialize in full-stack development,
-              primarily using JavaScript, React for building dynamic user
-              interfaces, and Node.js for back-end services. Over the years, I
-              have honed my skills in building responsive and high-performance
-              web applications, always aiming to provide the best user
-              experience. My journey in web development has been driven by a
-              desire to continuously learn and stay updated with the latest
-              industry trends and tools.`,
+    description: `I'm Hamidreza, a 22-year-old web developer`,
   },
 };
